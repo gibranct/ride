560eb13cebc410e662f7d80655d44bd0ccff8ee3
Gibran Tavares <mac01gibran@gmail.com>
2024-12-07 09:24:24 -0300

    add table position

--- init.sql
+++ init.sql
@@ -26,3 +26,11 @@ create table gct.ride (
 	to_long numeric,
 	date timestamp
 );
+
+create table gct.position (
+	position_id uuid,
+	ride_id uuid,
+	lat numeric,
+	long numeric,
+	date timestamp
+);
